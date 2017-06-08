bba9480dcf806e4f587d18e1b1904d760d28b367
mandelangatia <mandelangatia.com>
2017-06-08 14:47:25 +0300

    changed code to return false for numbers more than 3999

--- js/scripts.js
+++ js/scripts.js
@@ -3,13 +3,17 @@ var convertToRoman = function(num){
   var decimalValue = [1000,900,500,400,100,90,50,40,10,10,9,5,4,1]
   var romanNumeral = ['M','CM','D','CD','C','XC','L','XL','X','LX','V','IV','V','I']
   var romanized = ' ';
+  if (num <=3999) {
   for(var index=0; index<decimalValue.length; index++){
     while(decimalValue[index]<=num){
       romanized += romanNumeral[index];
       num -= decimalValue[index];
     }
   }
-  return romanized;
+  return romanized;}
+  else {
+    return false;
+  }
 }
 // user interface logic
 $(document).ready(function() {
