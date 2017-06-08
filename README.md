382609116c75f2a061f8de72d335de051f35f2d0
mandelangatia <mandelangatia.com>
2017-06-08 12:41:46 +0300

    created the decimal to roman number converter

---
+++ README.md
@@ -0,0 +1,10 @@
+## Author
+Mandela Kiboi Ngatia
+## Project Name
+decimal to Roman Numeral Converter
+## Specifications
+You cant have more than three roman numerals together <br>
+Create two arrays one with roman numbers and one with decimal values <br>
+
+## Technologies
+The languages used are HTML, CSS, JavaScript
---
+++ js/scripts.js
@@ -0,0 +1,22 @@
+// Business logic
+var convertToRoman = function(num){
+  var decimalValue = [1000,900,500,400,100,90,50,40,10,10,9,5,4,1]
+  var romanNumeral = ['M','CM','D','CD','C','XC','L','XL','X','LX','V','IV','V','I']
+  var romanized = ' ';
+  for(var index=0; index<decimalValue.length; index++){
+    while(decimalValue[index]<=num){
+      romanized += romanNumeral[index];
+      num -= decimalValue[index];
+    }
+  }
+  return romanized;
+}
+// user interface logic
+$(document).ready(function() {
+  $("form#roman-number").submit(function(event) {
+    event.preventDefault();
+    var userInput = parseInt($("input#number").val());
+    var result = convertToRoman(userInput);
+    $("#showResult").text(result);
+  });
+});
